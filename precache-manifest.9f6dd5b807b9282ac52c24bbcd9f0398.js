fcf6ac385ad7b537809a652b39dfa5fc19b121f5
LYH94 <49682614+LYH94@users.noreply.github.com>
2022-03-10 14:46:10 +0000

    Deploying to gh-pages from @ LYH94/Vue-Store@f0dc61e657255fde38438c6dd57167bdab82f8e3 🚀

---
+++ precache-manifest.9f6dd5b807b9282ac52c24bbcd9f0398.js
@@ -0,0 +1,302 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb31fc14ff684ccf86f5385136cc1f83",
+    "url": "about/flavor1.jpg"
+  },
+  {
+    "revision": "1d6d676fd82371aae5b763d02f5ddff2",
+    "url": "about/flavor2.jpg"
+  },
+  {
+    "revision": "d854dfde9c9d8af1ecd9fc0ac01d9753",
+    "url": "about/flavor3.jpg"
+  },
+  {
+    "revision": "27f62c8cbc8cfb7e3f42d5abb6d6fa06",
+    "url": "about/flavor4.jpg"
+  },
+  {
+    "revision": "4dcf5a85d8752d603880815629018cc3",
+    "url": "about/flavor5.jpg"
+  },
+  {
+    "revision": "45ab2dcbf4732647bd7feaa24b62d7da",
+    "url": "about/powder1.png"
+  },
+  {
+    "revision": "48dcc460f11b3faf1c2412f93ed102f7",
+    "url": "about/powder2.png"
+  },
+  {
+    "revision": "60fd4849cac622eb318a139623cc021c",
+    "url": "about/powder3.png"
+  },
+  {
+    "revision": "9176b35de59980042cdd75f9267d8c9b",
+    "url": "about/powder4.png"
+  },
+  {
+    "revision": "161156c472b90f60899f75e459835b09",
+    "url": "about1.jpg"
+  },
+  {
+    "revision": "a49c279725a21fa0cb016dab8138e155",
+    "url": "banner/b1.jpg"
+  },
+  {
+    "revision": "3f946699748f0e3b37ccb359a382015f",
+    "url": "banner/b2.jpg"
+  },
+  {
+    "revision": "99c11b496cc4eb0536d5ae5d2d732b8c",
+    "url": "banner/b3.jpg"
+  },
+  {
+    "revision": "cff571ca97b8cf02a0830bd51f1726cc",
+    "url": "banner/b4.jpg"
+  },
+  {
+    "revision": "ee4c92acdf172d21fba6b24a20f8eb62",
+    "url": "banner/b5.jpg"
+  },
+  {
+    "revision": "5154409ebc2ffd781480",
+    "url": "css/about.e0bcce10.css"
+  },
+  {
+    "revision": "2492ca1a81d975e6c21a",
+    "url": "css/app.4a6927a5.css"
+  },
+  {
+    "revision": "30d67965a9ab99ef9908",
+    "url": "css/cart.5c3c091d.css"
+  },
+  {
+    "revision": "4b97af3a7b9ec4aec81e",
+    "url": "css/chunk-vendors.4beb5d45.css"
+  },
+  {
+    "revision": "0c6f0e41ef8a62b05fe5",
+    "url": "css/dashboard.4e44207e.css"
+  },
+  {
+    "revision": "3ee9692157404b54e719",
+    "url": "css/item.80b37be6.css"
+  },
+  {
+    "revision": "5d5cf1244246d7683ca4",
+    "url": "css/login.beaac12e.css"
+  },
+  {
+    "revision": "d2c8de50bea68223f080",
+    "url": "css/orders.eea2b3f0.css"
+  },
+  {
+    "revision": "a3d439637998e30deccc",
+    "url": "css/products.044eb2dd.css"
+  },
+  {
+    "revision": "8e6f941829462629bff7",
+    "url": "css/reg.139327b0.css"
+  },
+  {
+    "revision": "a5921de2ebe1a80a37d3",
+    "url": "css/shop.f214deb7.css"
+  },
+  {
+    "revision": "ab2f358769a5603432739c296dddd527",
+    "url": "img/about1.ab2f3587.jpg"
+  },
+  {
+    "revision": "b01749118bc1d6a99e1eb5daefca3b0c",
+    "url": "img/about2.b0174911.jpg"
+  },
+  {
+    "revision": "c46b84a9f1b26c0688297f41ff2ed205",
+    "url": "img/about3.c46b84a9.jpg"
+  },
+  {
+    "revision": "92d76de14d076ba03d5a67ed5182642b",
+    "url": "img/about_animated.92d76de1.svg"
+  },
+  {
+    "revision": "683cab0827eb25eb560b16c0cf158923",
+    "url": "img/bg.683cab08.jpg"
+  },
+  {
+    "revision": "a1b2db8614d5af761d4326ac5158d072",
+    "url": "img/homepic01.a1b2db86.png"
+  },
+  {
+    "revision": "a5b5d4166c094c433226b2d93077cab6",
+    "url": "img/iconfont.a5b5d416.svg"
+  },
+  {
+    "revision": "2589340f0ce0f06e4286a6d9ed78126a",
+    "url": "img/intro2.2589340f.png"
+  },
+  {
+    "revision": "ed06581ff16ea398fcdc1d05c48a80be",
+    "url": "img/intro3.ed06581f.png"
+  },
+  {
+    "revision": "ff0fbcbc2cd15a84ab8781a6a7483a3a",
+    "url": "img/itempic.ff0fbcbc.jpg"
+  },
+  {
+    "revision": "6d33ac798c1e8fcf2e22a315c88ef8c6",
+    "url": "img/logo.6d33ac79.svg"
+  },
+  {
+    "revision": "57ffb0ee7b650a200f38e1555bbafddf",
+    "url": "img/pic01.57ffb0ee.jpg"
+  },
+  {
+    "revision": "1e95ce57f40ed9a34b877d0c97935715",
+    "url": "img/row0.1e95ce57.jpg"
+  },
+  {
+    "revision": "f892185c32b7324e1a8fa6348b61f386",
+    "url": "img/row1.f892185c.jpg"
+  },
+  {
+    "revision": "55bcc1130cd1ef35522f07b9a170402e",
+    "url": "img/row2.55bcc113.jpg"
+  },
+  {
+    "revision": "2f9f88b71765e80251e2ad52a3f6929e",
+    "url": "img/row3.2f9f88b7.jpg"
+  },
+  {
+    "revision": "5d54986cd83b4ba39212486f956063dc",
+    "url": "img/row4.5d54986c.jpg"
+  },
+  {
+    "revision": "cb3d37d38bf15b2e0e7b7b7fc66fbc89",
+    "url": "img/svgbg.cb3d37d3.jpg"
+  },
+  {
+    "revision": "37187b744abb7a4a5622f4ef0c0726c2",
+    "url": "index.html"
+  },
+  {
+    "revision": "5154409ebc2ffd781480",
+    "url": "js/about.58fbbf63.js"
+  },
+  {
+    "revision": "2492ca1a81d975e6c21a",
+    "url": "js/app.cd7cbe4c.js"
+  },
+  {
+    "revision": "30d67965a9ab99ef9908",
+    "url": "js/cart.b65dd4fd.js"
+  },
+  {
+    "revision": "4b97af3a7b9ec4aec81e",
+    "url": "js/chunk-vendors.fe948aac.js"
+  },
+  {
+    "revision": "0c6f0e41ef8a62b05fe5",
+    "url": "js/dashboard.afef57ba.js"
+  },
+  {
+    "revision": "3ee9692157404b54e719",
+    "url": "js/item.dccb1644.js"
+  },
+  {
+    "revision": "5d5cf1244246d7683ca4",
+    "url": "js/login.ebdf5f4c.js"
+  },
+  {
+    "revision": "d2c8de50bea68223f080",
+    "url": "js/orders.b58c4599.js"
+  },
+  {
+    "revision": "a3d439637998e30deccc",
+    "url": "js/products.6f2fc1bd.js"
+  },
+  {
+    "revision": "8e6f941829462629bff7",
+    "url": "js/reg.b2652cd0.js"
+  },
+  {
+    "revision": "a5921de2ebe1a80a37d3",
+    "url": "js/shop.2535aca0.js"
+  },
+  {
+    "revision": "dd9f4e1225183309e549f2fae850cf5d",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b90d6d8eec30587c8e7e5b38d58fce04",
+    "url": "product/01.jpg"
+  },
+  {
+    "revision": "8d3958e6b1d95ede1bf6e13c80790994",
+    "url": "product/02.jpg"
+  },
+  {
+    "revision": "8edf95bbef78c3ae7876e3660a0763a9",
+    "url": "product/03.jpg"
+  },
+  {
+    "revision": "3d0fa2c56a3cf427d110778eb7006561",
+    "url": "product/05.jpg"
+  },
+  {
+    "revision": "ececeba08514a1f62613c9bfd2d2e9e0",
+    "url": "product/06.jpg"
+  },
+  {
+    "revision": "33be284e79337ee339b09486f4ea632a",
+    "url": "product/07.jpg"
+  },
+  {
+    "revision": "7f43a140b23a24d2a2f4f635dc475f6a",
+    "url": "product/08.jpg"
+  },
+  {
+    "revision": "ed2826a274317b1eb2cf849a6c3f918e",
+    "url": "product/09.jpg"
+  },
+  {
+    "revision": "5394c1fc813f186d7df764b4d0e96305",
+    "url": "product/10.jpg"
+  },
+  {
+    "revision": "cf2355e010f296b3bdc8a2c891905e37",
+    "url": "product/11.jpg"
+  },
+  {
+    "revision": "c525821d51af5ed1ad2b92ddcb01068e",
+    "url": "product/12.jpg"
+  },
+  {
+    "revision": "77b4cea7c8235bdfd82fa81cefcf419a",
+    "url": "product/13.jpg"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "23cb9c04ac5e915c2a7a1feab6919e14",
+    "url": "wood/wood1.jpg"
+  },
+  {
+    "revision": "5293c20836cd310764ba6fbb4c2577c0",
+    "url": "wood/wood2.jpg"
+  },
+  {
+    "revision": "37c82e8f70d08f0e1311b6f8fe11b8ac",
+    "url": "wood/wood3.jpg"
+  },
+  {
+    "revision": "19280a047f5c65a0ef401b8083727504",
+    "url": "wood/wood4.jpg"
+  },
+  {
+    "revision": "c88407f4aee9b7f29806b6f29b932940",
+    "url": "wood/wood5.jpg"
+  }
+]);
